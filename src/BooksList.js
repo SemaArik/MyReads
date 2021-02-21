a436ec04d7e5ec1e2ecb4dac420f358179c1677d
SEMA ARIK <sema.arik@turkcell.com.tr>
2021-02-21 22:28:39 +0300

    [Sema] proje

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import ShelfChanger from './ShelfChanger';
 
 class BooksList extends Component {
   static propTypes = {
     bookListArray: PropTypes.array.isRequired,
    /* onDeleteContact: PropTypes.func.isRequired,*/
   }
+
+  
   state = {
     query: ''
   }
@@ -22,7 +25,6 @@ class BooksList extends Component {
     const { query } = this.state
     const { bookListArray, onDeleteContact } = this.props
 
-   
 
     return (    
                   <div className="bookshelf-books">
@@ -30,23 +32,17 @@ class BooksList extends Component {
                     <ol className="books-grid">
 
                           
-                    {bookListArray.map((book1) => (
+                    {bookListArray.map((bookItem) => (
                       <li>
                       <div className="book">
                         <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book1.imageLinks.thumbnail})` }}></div>
-                          <div className="book-shelf-changer">
-                            <select>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading</option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
+                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookItem.imageLinks.thumbnail})` }}></div>
+                          <ShelfChanger currentShelf={bookItem.shelf}  />
                         </div>
-                        <div className="book-title">{book1.title}</div>
-                        <div className="book-authors">{book1.authors}  </div>
+                        <div className="book-title">{bookItem.title}</div>
+                        { bookItem.authors ? (bookItem.authors.map(author=>(
+                          <div key={author} className="book-authors">{author}</div>
+                      ))):''}
                       </div>
                       </li>
                     
---
+++ src/ShelfChanger.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+const ShelfChanger = props => {
+  const { currentShelf } = props;
+  
+  const updateShelf = (movedShelf) => {
+      props.updateShelf(movedShelf);
+  }
+  return ( 
+      <div className="book-shelf-changer">
+          <select value={currentShelf ? currentShelf:'none'} onChange={(event) => updateShelf(event.target.value)}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want To Read</option>
+              <option value="read">Read</option>
+               <option value="none">None</option>
+          </select>
+      </div>
+  );
+}
+
+ShelfChanger.propTypes = {
+  currentShelf : PropTypes.string,
+  isSearchPage : PropTypes.bool
+}
+
+export default ShelfChanger
